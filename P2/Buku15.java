2b735ceb7ec6fd65b6f4ee4571b4f50977d93c08
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-20 11:12:05 +0700

    Latihan Praktikum

--- P2/Buku15.java
+++ P2/Buku15.java
@@ -2,19 +2,23 @@ package P2;
 
 public class Buku15 {
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, sold, total;
 
     void tampilInformasi() {
-        System.out.println("Judul : " + judul);
-        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Judul          : " + judul);
+        System.out.println("Pengarang      : " + pengarang);
         System.out.println("Jumlah halaman : " + halaman);
-        System.out.println("Sisa stok : " + stok);
-        System.out.println("Harga : Rp " + harga);
+        System.out.println("Sisa stok      : " + stok);
+        System.out.println("Harga          : Rp " + harga);
+        System.out.println("Harga total    : Rp " + total);
+        System.out.println("Diskon         : Rp " + hitungDiskon());
+        System.out.println("Harga bayar    : Rp " + hitungHargaBayar());
     }
 
     void terjual(int jml) {
         if (stok>0) {
             stok-=jml;
+            sold = jml;
         }
     }
 
@@ -24,6 +28,31 @@ public class Buku15 {
 
     void gantiHarga(int hrg) {
         harga = hrg;
+    }
+
+    int hitungHargaTotal() {
+        total = harga * sold;
+        return total;
+    }
+
+    int hitungDiskon() {
+        if (hitungHargaTotal() > 150000) {
+            double diskon = hitungHargaTotal() * 0.12;
+            int diskon2 = (int) diskon;
+            return diskon2;
+        } else if (hitungHargaTotal() > 75000 && hitungHargaTotal() <= 150000) {
+            double diskon = hitungHargaTotal() * 0.05;
+            int diskon2 = (int) diskon;
+            return diskon2;
+        } else {
+            double diskon = 0;
+            int diskon2 = (int) diskon;
+            return diskon2;
+        }
+    }
+
+    int hitungHargaBayar() {
+        return hitungHargaTotal() - hitungDiskon();
     }
 
     public Buku15() {
--- P2/BukuMain15.java
+++ P2/BukuMain15.java
@@ -3,23 +3,32 @@ package P2;
 public class BukuMain15 {
     public static void main(String[] args) {
         
-        Buku15 bk1 = new Buku15();
-        bk1.judul = "Today Ends Tomorrow Comes";
-        bk1.pengarang = "Denanda Pratiwi";
-        bk1.halaman = 198;
-        bk1.stok = 13;
-        bk1.harga = 71000;
+        // Buku15 bk1 = new Buku15();
+        // bk1.judul = "Today Ends Tomorrow Comes";
+        // bk1.pengarang = "Denanda Pratiwi";
+        // bk1.halaman = 198;
+        // bk1.stok = 13;
+        // bk1.harga = 71000;
 
-        bk1.tampilInformasi();
-        bk1.terjual(5);
-        bk1.gantiHarga(60000);
-        bk1.tampilInformasi();
+        // bk1.tampilInformasi();
+        // bk1.terjual(5);
+        // bk1.gantiHarga(60000);
+        // bk1.tampilInformasi();
 
         Buku15 bk2 = new Buku15("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        System.out.println("======Sebelum terjual======");
+        bk2.tampilInformasi();
         bk2.terjual(11);
+        bk2.hitungHargaTotal();
+        System.out.println("======Setelah terjual======");
         bk2.tampilInformasi();
 
         Buku15 Kiak = new Buku15("Attack on Titan", "Raffi Ahmad", 150, 10, 60000);
+        System.out.println("======Sebelum terjual======");
+        Kiak.tampilInformasi();
+        Kiak.terjual(6);
+        Kiak.hitungHargaTotal();
+        System.out.println("======Setelah terjual======");
         Kiak.tampilInformasi();
     }
 }
