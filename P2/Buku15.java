58f8b7b6d574164f3aa5346e0502d6f8e838c44d
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-20 08:32:14 +0700

    Percobaan 1 : Modifikasi kode pada soal

--- P2/Buku15.java
+++ P2/Buku15.java
@@ -5,15 +5,17 @@ public class Buku15 {
     int halaman, stok, harga;
 
     void tampilInformasi() {
-        System.out.println("Judul " + judul);
-        System.out.println("Pengarang " + pengarang);
-        System.out.println("Jumlah halaman " + halaman);
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah halaman : " + halaman);
         System.out.println("Sisa stok : " + stok);
         System.out.println("Harga : Rp " + harga);
     }
 
     void terjual(int jml) {
-        stok-=jml;
+        if (stok>0) {
+            stok-=jml;
+        }
     }
 
     void restock(int jml) {
