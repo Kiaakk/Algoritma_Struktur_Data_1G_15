ee8bc3117129bdeeb66748854a024f531f55e8b2
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-27 22:40:22 +0700

    Percobaan 3 : Pertanyaan 5

--- P3/ArraySegitiga/ArraySegitiga15.java
+++ P3/ArraySegitiga/ArraySegitiga15.java
@@ -7,5 +7,13 @@ public class ArraySegitiga15 {
         sgArray[1] = new Segitiga15(20, 10);
         sgArray[2] = new Segitiga15(15, 6);
         sgArray[3] = new Segitiga15(25, 10);
+
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("Segitiga ke-" + i + ":");
+            System.out.println("Alas: " + sgArray[i].alas + ", Tinggi: " + sgArray[i].tinggi);
+            System.out.println("Keliling: " + sgArray[i].hitungKeliling());
+            System.out.println("Luas: " + sgArray[i].hitungLuas());
+            System.out.println();
+        }
     }
 }
