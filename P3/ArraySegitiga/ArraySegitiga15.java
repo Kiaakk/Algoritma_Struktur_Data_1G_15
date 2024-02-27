8f3f419decdcd02647ef988fdbb7a51f9804d263
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-27 22:36:36 +0700

    Percobaan 3 : Pertanyaan 4

---
+++ P3/ArraySegitiga/ArraySegitiga15.java
@@ -0,0 +1,11 @@
+package P3.ArraySegitiga;
+
+public class ArraySegitiga15 {
+    public static void main(String[] args) {
+        Segitiga15 sgArray[] = new Segitiga15[4];
+        sgArray[0] = new Segitiga15(10, 4);
+        sgArray[1] = new Segitiga15(20, 10);
+        sgArray[2] = new Segitiga15(15, 6);
+        sgArray[3] = new Segitiga15(25, 10);
+    }
+}
