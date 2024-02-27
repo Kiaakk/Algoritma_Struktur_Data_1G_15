5a400e20e7c99337d8cb2a328320dc336de874a5
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-27 20:58:25 +0700

    Percobaan 2 : Pertanyaan 4

--- P3/ArrayObjects/ArrayObjects15.java
+++ P3/ArrayObjects/ArrayObjects15.java
@@ -3,14 +3,17 @@ package P3.ArrayObjects;
 import java.util.Scanner;
 
 public class ArrayObjects15 {
-
+    
     static Scanner kiak = new Scanner(System.in);
     public static void main(String[] args) {
-        PersegiPanjang15[] ppArray = new PersegiPanjang15[3];
-        Persegi15[] pgArray = new Persegi15[100];
-        pgArray[5].sisi = 20;
+        int panjangArray;
+        System.out.print("Banyak persegi panjang (array) : ");
+        panjangArray = kiak.nextInt();
+        PersegiPanjang15[] ppArray = new PersegiPanjang15[panjangArray];
+        // Persegi15[] pgArray = new Persegi15[100];
+        // pgArray[5].sisi = 20;
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < panjangArray; i++) {
             ppArray[i] = new PersegiPanjang15();
             System.out.println("Persegi panjang ke-" + i);
             System.out.print("Masukkan panjang: ");
@@ -19,7 +22,7 @@ public class ArrayObjects15 {
             ppArray[i].lebar = kiak.nextInt();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < panjangArray; i++) {
             System.out.println("Persegi panjang ke-" + i);
             System.out.println("Panjang : " + ppArray[i].panjang + ", lebar : " + ppArray[i].lebar);
         }
