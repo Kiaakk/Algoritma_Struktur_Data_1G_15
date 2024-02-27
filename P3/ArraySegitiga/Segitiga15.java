33992f3bfb8819d72e9fb676ec9c64e7d2adab2b
Kiaakk <krisna.wiliam26@gmail.com>
2024-02-27 22:26:02 +0700

    Percobaan 3 : Pertanyaan 2

---
+++ P3/ArraySegitiga/Segitiga15.java
@@ -0,0 +1,12 @@
+package P3.ArraySegitiga;
+
+public class Segitiga15 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga15(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+
+}
