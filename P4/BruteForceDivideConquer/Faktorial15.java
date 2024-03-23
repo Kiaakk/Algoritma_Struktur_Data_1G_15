bbf138de5886779e4b4df00125c111752b3a7af6
Kiaakk <krisna.wiliam26@gmail.com>
2024-03-23 14:16:06 +0700

    Percobaan 1: Pertanyaan 2

--- P4/BruteForceDivideConquer/Faktorial15.java
+++ P4/BruteForceDivideConquer/Faktorial15.java
@@ -4,9 +4,16 @@ public class Faktorial15 {
     int nilai;
 
     int faktorialBF(int n) {
+        // int faktor = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     faktor = faktor * i;
+        // }
+        // return faktor;
         int faktor = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             faktor = faktor * i;
+            i++;
         }
         return faktor;
     }
