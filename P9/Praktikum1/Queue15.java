1b4b79b4b235da7a0438ca6974f4740afc53726d
Kiaakk <krisna.wiliam26@gmail.com>
2024-04-30 18:57:38 +0700

    Percobaan 1: Pertanyaan 7

--- P9/Praktikum1/Queue15.java
+++ P9/Praktikum1/Queue15.java
@@ -65,6 +65,7 @@ public class Queue15 {
     void Enqueue(int dt) {
         if (IsFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         } else {
             if (IsEmpty()) {
                 front = rear = 0;
@@ -84,6 +85,7 @@ public class Queue15 {
         int dt = 0;
         if (IsEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
