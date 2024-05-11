c0386fba601e877b4f00aad650d53cb8f19aa092
Kiaakk <krisna.wiliam26@gmail.com>
2024-05-12 00:02:07 +0700

    Percobaan 2: Modifikasi Elemen pada Single Linked List

--- P10/SingleLinkedList15.java
+++ P10/SingleLinkedList15.java
@@ -75,4 +75,91 @@ public class SingleLinkedList15 {
             }
         }
     }
+
+    int getData(int index) {
+        Node15 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    int indexOf(int key) {
+        Node15 tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        } 
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kodong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node15 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            } 
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else {
+            Node15 temp = head;
+            while (temp != null) {
+                if (temp.data == key && temp == head) {
+                    removeFirst();
+                    break;
+                } else if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    void removeAt(int index) {
+        if (index < 0) {
+            System.out.println("Indeks nya salah");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node15 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                if (temp.next == null) {
+                    temp = temp.next;
+                }
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
