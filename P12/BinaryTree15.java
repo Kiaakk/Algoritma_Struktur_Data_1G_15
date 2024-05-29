512c425094c2d17ec78c1482fec1f415a4147d2c
Kiaakk <krisna.wiliam26@gmail.com>
2024-05-30 00:58:41 +0700

    Percobaan 1:  Binary Search Tree

---
+++ P12/BinaryTree15.java
@@ -0,0 +1,167 @@
+package P12;
+
+public class BinaryTree15 {
+
+    Node15 root;
+
+    BinaryTree15() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    void add(int data) {
+        if (isEmpty()) {
+            root = new Node15(data);
+        } else {
+            Node15 current = root;
+            while (true) {
+                if (data < current.data) {
+                    if (current.left != null) {
+                        current = current.left;
+                    } else {
+                        current.left = new Node15(data);
+                        break;
+                    }
+                } else if (data > current.data) {
+                    if (current.right != null) {
+                        current = current.right;
+                    } else {
+                        current.right = new Node15(data);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data) {
+        boolean result = false;
+        Node15 current = root;
+        while (current != null) {
+            if (current.data == data) {
+                result = true;
+                break;
+            } else if (current.data < data) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node15 node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node15 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    void traverseIndOrder(Node15 node) {
+        if (node != null) {
+            traverseIndOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseIndOrder(node.right);
+        }
+    }
+
+    Node15 getSuccessor(Node15 del) {
+        Node15 successor = del.right;
+        Node15 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree is empty");
+            return;
+        }
+        Node15 parent = root;
+        Node15 current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (current.data == data) {
+                break;
+            } else if (data < current.data) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (data > current.data) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        } else {
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {
+                Node15 successor = getSuccessor(current);
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                }
+                successor.left = current.left;
+            }
+        }
+    }
+
+}
---
+++ P12/Node15.java
@@ -0,0 +1,15 @@
+package P12;
+
+public class Node15 {
+    int data;
+    Node15 left;
+    Node15 right;
+
+    Node15() {
+
+    }
+
+    Node15(int data) {
+        this.data = data;
+    }
+}
