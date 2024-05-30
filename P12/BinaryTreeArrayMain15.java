8a9ae2da8b19693eadd3428c25cc8029241f83f8
Kiaakk <krisna.wiliam26@gmail.com>
2024-05-30 16:13:27 +0700

    Percobaan 2: Binary Tree Array

---
+++ P12/BinaryTreeArrayMain15.java
@@ -0,0 +1,14 @@
+package P12;
+
+public class BinaryTreeArrayMain15 {
+    public static void main(String[] args) {
+        
+        BinaryTreeArray15 bta = new BinaryTreeArray15();
+        int[] data = {6,4,8,3,5,7,9,0,0,0};
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+        System.out.print("\nInOrder Traversal :  ");
+        bta.traverseInOrder(0);
+        System.out.println("\n");
+    }
+}
